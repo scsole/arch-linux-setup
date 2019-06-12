e98f438526bbd555c892f2838c30d52d68852261
scsole <20161450+scsole@users.noreply.github.com>
2019-06-12 21:10:04 +1200

    Update 800-packages.sh

--- 800-packages.sh
+++ 800-packages.sh
@@ -5,9 +5,12 @@
 #
 
 PKGS=(
-    # Utilities
+    # Terminal Utilities
     'xdg-user-dirs'         # Manage user directories
     'mlocate'               # File searching utility
+    'git'                   # Distributed version control system
+    'vim'                   # A highly configurable text editor
+    'tree'                  # Directory listing program
 )
 
 sudo pacman -S "${PKGS[@]}" --needed
