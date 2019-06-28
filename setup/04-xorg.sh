ca62b2bfafb0755067202d58a2e628a18854f1e3
scsole <20161450+scsole@users.noreply.github.com>
2019-06-28 19:53:27 +1200

    Added NVIDIA hook

--- setup/04-xorg.sh
+++ setup/04-xorg.sh
@@ -34,3 +34,22 @@ Section "InputClass"
     Option "NaturalScrolling" "true"
 EndSection
 EOF
+
+# Make sure initramfs is updated after a NVIDIA driver update
+sudo tee /etc/pacman.d/hooks/nvidia.hook <<EOF
+[Trigger]
+Operation=Install
+Operation=Upgrade
+Operation=Remove
+Type=Package
+Target=nvidia
+Target=linux
+# Change the linux part above and in the Exec line if a different kernel is used
+
+[Action]
+Description=Update Nvidia module in initcpio
+Depends=mkinitcpio
+When=PostTransaction
+NeedsTargets
+Exec=/bin/sh -c 'while read -r trg; do case $trg in linux) exit 0; esac; done; /usr/bin/mkinitcpio -P'
+EOF
