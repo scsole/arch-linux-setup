edb12415d93f8bcd51b9b5d2fa0a5c20cd67727b
scsole <20161450+scsole@users.noreply.github.com>
2020-02-14 22:31:04 +1300

    changed script selection

--- 00-runall.sh
+++ 00-runall.sh
@@ -3,12 +3,12 @@
 # Run everything
 chmod +x ./*/*.sh ./*.sh
 
-./01-network.sh
+#./01-network.sh
 ./02-audio.sh
 ./03-bluetooth.sh
 ./04-xorg.sh
-#./05-i3.sh
-./05-kde.sh
+./05-i3.sh
+#./05-kde.sh
 ./06-printer.sh
 ./07-packages.sh
 ./08-packages-multilib.sh
