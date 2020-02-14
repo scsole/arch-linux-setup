7c1e522ade4adf3ea64ad8fc2f6b10b8d2cfb7ea
scsole <20161450+scsole@users.noreply.github.com>
2020-02-14 22:29:35 +1300

    added mDNS and LLMNR

--- setup/01-network.sh
+++ setup/01-network.sh
@@ -4,10 +4,6 @@
 # Network Setup
 #
 
-# TODO:
-# - mDNS
-# - LLMNR
-
 # Enable services
 sudo systemctl enable systemd-resolved.service
 sudo systemctl enable systemd-timesyncd.service
@@ -17,7 +13,30 @@ sudo systemctl enable ufw.service
 # Use systemd-resolved for domain name resolution
 sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 
+# Enable mDNS resolver
+sudo tee /etc/NetworkManager/conf.d/mdns.conf <<EOF
+[connection]
+connection.mdns=1
+EOF
+
+# Create firewall profiles for mDNS and LLMNR
+sudo tee /etc/ufw/applications.d/ufw-zeroconf<<EOF
+[mDNS]
+title=mDNS
+description=Multicast DNS hostname resolution
+ports=5353/udp
+
+[LLMNR]
+title=LLMNR
+description=Link-Local Multicast Name Resolution
+ports=5355/tcp|5355/udp
+EOF
+
 # Enable firewall and deny all by default
 sudo systemctl start ufw.service
 sudo ufw default deny
 sudo ufw enable
+
+# Allow mDNS and LLMNR through the firewall
+sudo ufw allow mDNS
+sudo ufw allow LLMNR
