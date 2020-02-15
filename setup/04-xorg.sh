6fc46be13724abbcce9f8c5b449847bd8f37ccc3
scsole <20161450+scsole@users.noreply.github.com>
2020-02-15 23:22:27 +1300

    Formatting changes

--- setup/04-xorg.sh
+++ setup/04-xorg.sh
@@ -4,7 +4,7 @@
 # Xorg Setup
 #
 
-# Pointer libinput configuration
+# Pointer libinput Xorg configuration
 sudo tee /etc/X11/xorg.conf.d/30-pointer.conf <<EOF
 Section "InputClass"
 	Identifier "pointer"
@@ -15,7 +15,7 @@ Section "InputClass"
 EndSection
 EOF
 
-# Touchpad libinput configuration
+# Touchpad libinput Xorg configuration
 sudo tee /etc/X11/xorg.conf.d/30-touchpad.conf <<EOF
 Section "InputClass"
 	Identifier "touchpad"
@@ -52,17 +52,17 @@ EOF
 # Make sure initramfs is updated after a NVIDIA driver update
 sudo tee /etc/pacman.d/hooks/nvidia.hook <<EOF
 [Trigger]
-Operation = Install
-Operation = Upgrade
-Operation = Remove
-Type = Package
-Target = nvidia
-Target = linux
+Operation=Install
+Operation=Upgrade
+Operation=Remove
+Type=Package
+Target=nvidia
+Target=linux
 
 [Action]
-Description = Update Nvidia module in initcpio
-Depends = mkinitcpio
-When = PostTransaction
+Description=Update Nvidia module in initcpio
+Depends=mkinitcpio
+When=PostTransaction
 NeedsTargets
-Exec = /bin/sh -c 'while read -r trg; do case \$trg in linux) exit 0; esac; done; /usr/bin/mkinitcpio -P'
+Exec=/bin/sh -c 'while read -r trg; do case \$trg in linux) exit 0; esac; done; /usr/bin/mkinitcpio -P'
 EOF
