f2980ea37be12c13fb8e392e7e157362e8023c75
Sebastian Sole <sebastian.c.sole@gmail.com>
2020-02-28 16:19:46 +1300

    Added PA zeroconf support

--- 02-audio.sh
+++ 02-audio.sh
@@ -9,6 +9,7 @@ PKGS=(
     'alsa-plugins'              # Extra ALSA plugins
     'pulseaudio'                # PulseAudio sound server
     'pulseaudio-alsa'           # ALSA configuration for PulseAudio
+    'pulseaudio-zeroconf'       # Zeroconf support for PulseAudio
     'pamixer'                   # PulseAudio command line mixer
 )
 
