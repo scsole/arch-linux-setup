8414110abce7a7fcf136128d760006add37cd063
Sebastian Sole <sebastian.c.sole@gmail.com>
2020-02-28 18:30:51 +1300

    Removed Nvidia packages

---
+++ 04-display.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+# Display
+#
+
+PKGS=(
+    # Xorg display server
+    'xorg-server'               # Xorg X server
+    'xorg-apps'                 # Additional Xorg packages
+    'xorg-xinit'                # Xorg initialisation program
+
+    # Display drivers
+    'mesa'                      # Open-source OpenGL implementation
+    'xf86-video-intel'          # Intel video drivers
+    'vulkan-intel'              # Intel's Vulkan mesa driver
+    'vulkan-icd-loader'         # Vulkan Installable Client Driver (ICD) Loader
+
+    # Hardware video acceleration
+    'intel-media-driver'        # VA-API support for Broadwell+ iGPUs
+    'libva-utils'               # Intel VA-API Media Applications and Scripts for libva
+
+    # GPGPU
+    'intel-compute-runtime'     # Neo OpenCL runtime for Broadwell+ iGPUs
+    'clinfo'                    # List OpenCL platforms, devices present and ICD loader properties
+)
+
+printf "\n Installing Display Components\n\n"
+sudo pacman -S "${PKGS[@]}" --needed
+
+printf "\n Configuring Display Components\n\n"
+./setup/04-xorg.sh
+
+printf "\n Display Components Done! \n\n"
--- setup/04-display.sh
+++ setup/04-display.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Xorg Setup
+# Display Setup
 #
 
 # Pointer libinput Xorg configuration
@@ -50,19 +50,19 @@ EOF
 #EOF
 
 # Make sure initramfs is updated after a NVIDIA driver update
-sudo tee /etc/pacman.d/hooks/nvidia.hook <<EOF
-[Trigger]
-Operation=Install
-Operation=Upgrade
-Operation=Remove
-Type=Package
-Target=nvidia
-Target=linux
-
-[Action]
-Description=Update Nvidia module in initcpio
-Depends=mkinitcpio
-When=PostTransaction
-NeedsTargets
-Exec=/bin/sh -c 'while read -r trg; do case \$trg in linux) exit 0; esac; done; /usr/bin/mkinitcpio -P'
-EOF
+#sudo tee /etc/pacman.d/hooks/nvidia.hook <<EOF
+#[Trigger]
+#Operation = Install
+#Operation = Upgrade
+#Operation = Remove
+#Type = Package
+#Target = nvidia
+#Target = linux
+#
+#[Action]
+#Description = Update Nvidia module in initcpio
+#Depends = mkinitcpio
+#When = PostTransaction
+#NeedsTargets
+#Exec = /bin/sh -c 'while read -r trg; do case \$trg in linux) exit 0; esac; done; /usr/bin/mkinitcpio -P'
+#EOF
