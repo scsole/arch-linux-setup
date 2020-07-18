e6f6c178b30652e1a59fc7e8aab4b66d5f981b27
Sebastian Sole <sebastian.c.sole@gmail.com>
2020-07-18 16:32:40 +1200

    Added initial gnome install

---
+++ 05-gnome.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#
+# GNOME DE
+#
+
+PKGS=(
+
+    # Display manager
+    'gdm'                       # Display manager
+
+    # Base Gnome desktop
+    'gnome'                     # GNOME desktop and a subset of well-integrated applications
+
+    # Extra GNOME packages
+    'gnome-tweaks'              # GNOME Tweak Tool
+    'xorg-server-xwayland'      # Run X clients under Wayland
+)
+
+printf "\n Installing GNOME components\n\n"
+sudo pacman -S "${PKGS[@]}" --needed --noconfirm
+
+status=$?
+if [ $status -eq 0 ]
+then
+    printf "\n Beginning GNOME configuration\n\n"
+else
+    printf "\n GNOME components failed!\n\n"
+fi
+
+# Start Gnome Display Manager on boot
+sudo systemctl enable gdm.service
+
+# Set some preferences
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click 'true'
+
+printf "\n GNOME components done\n\n"
