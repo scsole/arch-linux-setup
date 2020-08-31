f56b64193741d21f1349083ebb557ae6da7a164b
Sebastian Sole <scsole@protonmail.com>
2020-08-31 20:01:13 +1200

    Added unmaintained comments

--- 00-runall.sh
+++ 00-runall.sh
@@ -5,14 +5,14 @@
 ./03-bluetooth.sh
 ./04-display.sh
 ./05-gnome.sh
-#./05-i3.sh
+#./05-i3.sh                 # Unmaintained
 ./05-sway.sh
-#./05-kde.sh
+#./05-kde.sh                # Unmaintained
 ./06-printer.sh
 ./07-packages.sh
 ./07-packages-flatpak.sh
-#./08-packages-multilib.sh
-#./09-packages-aur.sh
+#./08-packages-multilib.sh  # Unmaintained
+./09-packages-aur.sh
 ./10-system-setup.sh
 
 printf "\n Finished Setup! \n\n"
